5dc7ea5c282daafe1b30ea10ea1f75fe45027081
alinese <sunpods1997@gmail.com>
2021-06-14 22:31:47 +0900

    UV filter analysis v1

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly as pt
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
+import plotly.offline as py
+from pathlib import Path
+
+# Web Settings
+st.set_page_config(page_title="BASF Sales Analysis by Ellin", layout="wide")
+
+
+st.write("Hello, World")
